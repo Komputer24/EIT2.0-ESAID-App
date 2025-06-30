58f8f46b4c002c748e38109ae542360e79187be5
Kaleab Beteselassie <kbeteselassie@eit20.com>
2025-06-30 15:50:22 -0400

    v3 - added bars & footer

--- app/src/main/java/com/example/eit20_app/MainActivity.kt
+++ app/src/main/java/com/example/eit20_app/MainActivity.kt
@@ -6,13 +6,18 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -35,7 +40,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.eit20_app.ui.theme.EIT20_AppTheme
@@ -52,19 +60,28 @@ class MainActivity : ComponentActivity() {
 //                val backgroundColor = if (isPressed) Color.Cyan else Color.Blue
 
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Surface(
-                        modifier = Modifier.fillMaxSize(),
-                        color = MaterialTheme.colorScheme.background
+                    Column(
+                        modifier = Modifier
+                            .background(Color.Black)
+                            .fillMaxSize(),
                     ) {
+                        // HEADER
                         Column(
                             modifier = Modifier
-                                .fillMaxSize()
-                                .padding(paddingValues = innerPadding),
+                                .fillMaxWidth()
+                                .padding(
+                                    start = innerPadding.calculateStartPadding(LayoutDirection.Ltr),
+                                    top = innerPadding.calculateTopPadding(),
+                                    end = innerPadding.calculateEndPadding(LayoutDirection.Ltr),
+                                    bottom = 0.dp
+                                ),
+//                                .background(Color.Green),
                             horizontalAlignment = Alignment.CenterHorizontally
                         ){
                             Spacer(modifier = Modifier.height(15.dp))
                             ButtonRow()
 
+                            Spacer(modifier = Modifier.height(15.dp))
                             Row(
 //                                modifier = Modifier
 //                                    .background(Color.Red),
@@ -77,8 +94,7 @@ class MainActivity : ComponentActivity() {
                                 )
                                 Column(
                                     modifier = Modifier
-                                        .padding(paddingValues = innerPadding),
-                                        //.background(Color.Cyan),
+//                                        .background(Color.Cyan),
                                 ){
                                     Text(
                                         text = "FT",
@@ -89,6 +105,113 @@ class MainActivity : ComponentActivity() {
                                         fontSize = 20.sp,
                                     )
                                 }
+                            }
+                        }
+                        // BODY
+                        Column(
+                            modifier = Modifier
+//                                .background(Color.Yellow)
+                                .padding(horizontal = 8.dp)
+                                .fillMaxWidth(),
+                            verticalArrangement = Arrangement.Bottom
+                        ){
+                            Spacer(modifier = Modifier.height(15.dp))
+                            Box(
+                                modifier = Modifier
+                                    .width(LocalConfiguration.current.screenWidthDp.dp)  // 384 for my screen
+                                    .height(LocalConfiguration.current.screenWidthDp.dp / 3)
+                                    .border(BorderStroke(2.dp, Color.White))
+                                    .background(Color.Black),
+                            ) {
+                                Text(
+                                    "BAR 1",
+                                    modifier = Modifier.align(Alignment.Center)
+                                )
+                            }
+                            Box(
+                                modifier = Modifier
+                                    .width(LocalConfiguration.current.screenWidthDp.dp)  // 384 for my screen
+                                    .height(LocalConfiguration.current.screenWidthDp.dp / 3)
+                                    .border(BorderStroke(2.dp, Color.White))
+                                    .background(Color.Black),
+                            ) {
+                                Text(
+                                    "BAR 2",
+                                    modifier = Modifier.align(Alignment.Center)
+                                )
+                            }
+                            Box(
+                                modifier = Modifier
+                                    .width(LocalConfiguration.current.screenWidthDp.dp)  // 384 for my screen
+                                    .height(LocalConfiguration.current.screenWidthDp.dp / 3)
+                                    .border(BorderStroke(2.dp, Color.White))
+                                    .background(Color.Black),
+                            ) {
+                                Text(
+                                    "BAR 3",
+                                    modifier = Modifier.align(Alignment.Center)
+                                )
+                            }
+                            Spacer(modifier = Modifier.height(15.dp))
+                        }
+                        // FOOTER
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth() // Make the Column fill the entire screen
+                                .padding(horizontal = 8.dp)
+                                .height(90.dp),
+//                                .background(Color.Red),
+                            verticalAlignment = Alignment.Bottom // Push the Box to the bottom
+                        ){
+                            OutlinedButton(
+                                onClick = { selectedBtn() },
+                                modifier = Modifier
+                                    .size(width = 115.dp, height = 90.dp)
+                                    .padding(end = 4.dp),
+                                colors = ButtonDefaults.buttonColors(
+                                    containerColor =Color.Blue,
+                                    contentColor = Color.White,
+                                ),
+                                border = BorderStroke(2.dp, Color.White),
+                                shape = CutCornerShape(7.dp),
+                            ) {
+                                Image(
+                                    painterResource(id = R.drawable.setting_icon),
+                                    contentDescription = "Settings"
+                                )
+                            }
+                            Box(
+                                modifier = Modifier
+                                    .width(140.dp)  // 384 for my screen
+                                    .height(LocalConfiguration.current.screenWidthDp.dp / 3)
+                                    .border(BorderStroke(2.dp, Color.White)),
+                            ) {
+                                Text(
+                                    "EXC",
+                                    modifier = Modifier.align(Alignment.Center)
+                                )
+                            }
+                            OutlinedButton(
+                                onClick = { selectedBtn() },
+                                modifier = Modifier
+                                    .size(width = 115.dp, height = 90.dp)
+                                    .padding(start = 4.dp),
+                                colors = ButtonDefaults.buttonColors(
+                                    containerColor =Color.Blue,
+                                    contentColor = Color.White,
+                                ),
+                                border = BorderStroke(2.dp, Color.White),
+                                shape = CutCornerShape(7.dp),
+                            ) {
+//                                Image(
+//                                    painterResource(id = R.drawable.info_icon),
+//                                    contentDescription = "Info"
+//                                )
+                                Text(
+                                    "i",
+                                    fontFamily = FontFamily.Serif,
+                                    fontSize = 50.sp
+                                )
                             }
                         }
                     }
